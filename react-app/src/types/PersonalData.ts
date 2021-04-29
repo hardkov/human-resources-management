d6b519c16ba580f2e88e9799803271c46d58e089
hardkov <krzysztofh150@interia.pl>
2021-04-29 20:54:51 +0200

    Added appropriete types

---
+++ react-app/src/types/PersonalData.ts
@@ -0,0 +1,12 @@
+interface PersonalData {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  birthdate: string;
+  thumbnail: string[];
+}
+
+export default PersonalData;
