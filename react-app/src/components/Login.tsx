47d58d1ab5841468705c8d922a018beacb68b760
hardkov <krzysztofh150@interia.pl>
2021-04-30 20:44:57 +0200

    Connected login components to auth service

--- react-app/src/components/Login.tsx
+++ react-app/src/components/Login.tsx
@@ -1,29 +1,36 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import LoginForm from "./LoginForm";
-// import useForm from "../../hooks/useForm";
-// import { loginUser } from "../../services/authService";
+import LoginForm from './LoginForm';
+import { login } from '../services/authService';
+import ActionResult from '../types/ActionResult';
+
+interface Credentials {
+  username: string;
+  password: string;
+}
 
 const Login = () => {
-    // const [
-    //     isLoading,
-    //     errorMessage,
-    //     redirect,
-    //     handleCredentialsChange,
-    //     handleSubmit,
-    // ] = useForm(loginUser, { username: "", password: "" });
-    //
-    // if (redirect) return <Redirect to="/" />;
+  const [credentials, setCredentials] = useState<Credentials>({ username: '', password: '' });
+  const [redirect, setRedirect] = useState<boolean>(false);
+  const [error, setErrors] = useState<string>('');
 
-    return (
-        <LoginForm
-            // isLoading={isLoading}
-            // errorMessage={errorMessage}
-            // handleCredentialsChange={handleCredentialsChange}
-            // handleSubmit={handleSubmit}
-        />
-    );
+  const handleCredentialsChange = (event: React.FormEvent) => {
+    const target = event.target as HTMLInputElement;
+    setCredentials({ ...credentials, [target.name]: target.value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const result: ActionResult = await login(credentials.username, credentials.password);
+
+    if (result.success) setRedirect(true);
+    else if (result.errors) setErrors(result.errors[0]);
+  };
+
+  if (redirect) return <Redirect to="/home" />;
+  return <LoginForm handleSubmit={handleSubmit} handleCredentialsChange={handleCredentialsChange} error={error} />;
 };
 
 export default Login;
