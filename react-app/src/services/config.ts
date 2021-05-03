f0d3761c8a8ff9a8741c9e8968b655314d3775ea
Paweł Kocimski <pawelpoczta1201@o2.pl>
2021-05-03 23:31:43 +0200

    cosmetic change needed for LeaveService

--- react-app/src/services/config.ts
+++ react-app/src/services/config.ts
@@ -1,4 +1,5 @@
 const LOGIN_ENDPOINT = '/api/public/login';
 const USER_DATA_ENDPOINT = '/user';
+const LEAVE_ENDPOINT = '/leave';
 
-export { LOGIN_ENDPOINT, USER_DATA_ENDPOINT };
+export { LOGIN_ENDPOINT, USER_DATA_ENDPOINT, LEAVE_ENDPOINT };
