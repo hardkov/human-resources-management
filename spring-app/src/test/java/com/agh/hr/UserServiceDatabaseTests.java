6f4b0e41fe7742f5d06c9289016651523668acae
ChrisFlam3 <krzysmisan@gmail.com>
2021-05-03 21:36:09 +0200

    integration tests service-db

---
+++ spring-app/src/test/java/com/agh/hr/UserServiceDatabaseTests.java
@@ -0,0 +1,137 @@
+package com.agh.hr;
+import com.agh.hr.persistence.model.PersonalData;
+import com.agh.hr.persistence.model.User;
+import com.agh.hr.persistence.repository.UserRepository;
+import com.agh.hr.persistence.service.RoleService;
+import com.agh.hr.persistence.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
+
+import javax.validation.constraints.AssertTrue;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserServiceDatabaseTests {
+
+    private User userTest;
+    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+    private RoleService roleService;
+
+    @Autowired
+    public UserServiceDatabaseTests(UserService userService,PasswordEncoder passwordEncoder,RoleService roleService){
+        this.userService=userService;
+        this.passwordEncoder=passwordEncoder;
+        this.roleService=roleService;
+
+    }
+
+    @BeforeEach
+    public void setup(){
+        userTest = new UserTest();
+        PersonalData personalData= new ConvertersTests.PersonalDataTest();
+        personalData.setFirstname("Susan");
+        personalData.setLastname("Barrow");
+
+        userTest=User.builder()
+                .username("sus")
+                .passwordHash(passwordEncoder.encode("passw0rd"))
+                .enabled(true)
+                .authorities(Collections.singleton(roleService.userRole()))
+                .personalData(personalData)
+                .position("Tester")
+                .leaves(Collections.emptyList())
+                .bonuses(Collections.emptyList())
+                .delegations(Collections.emptyList())
+                .applications(Collections.emptyList()).build();
+
+    }
+    static class UserTest extends User {}
+
+    @Test
+    void testSaveUser(){
+        Optional<User> result=userService.saveUser(userTest);
+        assertEquals(Optional.of(userTest),result);
+    }
+
+    @Test
+    void testSuccessFindByUsername(){
+        assertAll(
+                ()->assertTrue(userService.saveUser(userTest).isPresent()),
+                ()->assertTrue(userService.findByUsername("sus").isPresent())
+        );
+    }
+
+    @Test
+    void testFailFindByUsername(){
+      assertFalse(userService.findByUsername("sus").isPresent());
+    }
+
+    @Test
+    void testGetById(){
+        Optional<User> result=userService.saveUser(userTest);
+        assertTrue(userService.getById(userTest.getId()).isPresent());
+
+    }
+
+    @Test
+    void testSuccessGetByFirstname(){
+        assertAll(
+                ()->assertTrue(userService.saveUser(userTest).isPresent()),
+                ()->assertEquals(1,userService.getByFirstname("Susan").size())
+        );
+
+    }
+
+    @Test
+    void testFailGetByFirstname(){
+        assertEquals(0,userService.getByFirstname("Susan").size());
+    }
+
+    @Test
+    void testSuccessGetByLastname(){
+        assertAll(
+                ()->assertTrue(userService.saveUser(userTest).isPresent()),
+                ()->assertEquals(1,userService.getByLastname("Barrow").size())
+        );
+
+    }
+
+    @Test
+    void testFailGetByLastname(){
+        assertEquals(0,userService.getByLastname("Barrow").size());
+    }
+
+    @Test
+    void testSuccessGetByFullName(){
+        assertAll(
+                ()->assertTrue(userService.saveUser(userTest).isPresent()),
+                ()->assertEquals(1,userService.getByFullName("Susan","Barrow").size())
+        );
+    }
+
+    @Test
+    void testFailGetByFullName(){
+        assertEquals(0,userService.getByFullName("Susan","Barrow").size());
+    }
+
+    @Test
+    void testDeleteUser(){
+        assertAll(
+                ()->userService.saveUser(userTest),
+                ()->userService.deleteUser(userTest.getId()),
+                ()->assertFalse(userService.getById(userTest.getId()).isPresent())
+        );
+    }
+
+}
