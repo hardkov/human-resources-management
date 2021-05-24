f282e1680bc021f306811a83b16df98306f8556f
Pinioo <jakpin1999@gmail.com>
2021-05-24 22:27:21 +0200

    Removed accidentally added endpoint name

--- react-app/src/services/config.ts
+++ react-app/src/services/config.ts
@@ -1,6 +1,5 @@
 const LOGIN_ENDPOINT = '/api/public/login';
 const USER_DATA_ENDPOINT = '/api/user';
 const LEAVE_ENDPOINT = '/api/leave';
-const EMPLOYEES_ENDPOINT = '/api/employees';
 
-export { LOGIN_ENDPOINT, USER_DATA_ENDPOINT, LEAVE_ENDPOINT, EMPLOYEES_ENDPOINT };
+export { LOGIN_ENDPOINT, USER_DATA_ENDPOINT, LEAVE_ENDPOINT };
