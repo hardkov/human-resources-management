6fa9ff9b55306d14de650f4085b547f02f04ed12
Pinioo <jakpin1999@gmail.com>
2021-05-27 18:21:16 +0200

    LeavedDTO validation added

--- spring-app/src/main/java/com/agh/hr/persistence/dto/LeaveDTO.java
+++ spring-app/src/main/java/com/agh/hr/persistence/dto/LeaveDTO.java
@@ -1,16 +1,28 @@
 package com.agh.hr.persistence.dto;
 
+import com.agh.hr.persistence.dto.validation.groups.UpdateGroup;
 import lombok.*;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class LeaveDTO {
+    @NotNull(groups = {UpdateGroup.class}, message = "Cannot be null")
     private Long id;
     private UserDTO user;
+
+    @NotNull(message = "Cannot be null")
     private LocalDate startDate;
+
+    @NotNull(message = "Cannot be null")
     private LocalDate endDate;
     private boolean paid;
     private byte[] thumbnail;
+
+    @AssertTrue(message = "Start date must be before end date")
+    private boolean isValidDates(){
+        return startDate.minusDays(1).isBefore(endDate);
+    }
 }
