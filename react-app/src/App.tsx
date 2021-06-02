eb8ea532f22047d6ae358fe9956204b0119070b1
hardkov <krzysztofH150@interia.pl>
2021-06-02 16:49:29 +0200

    Implemented home menu images caching

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,6 +4,19 @@ import { CssBaseline, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import Routes from './components/Routes';
 import defaultTheme from './theme/defaultTheme.json';
 
+// import images at high-level component in order to cache it
+/* eslint-disable */
+import applicationImg from '../assets/applications.jpg';
+import employeesImg from '../assets/employees.jpg';
+import logsImg from '../assets/logs.png';
+import myApplicationsImg from '../assets/myApplications.jpg';
+import createApplicationsImg from '../assets/createApplication.png';
+import earningsImg from '../assets/earnings.jpg';
+import myEarningsImg from '../assets/myEarnings.jpeg';
+import leavesImg from '../assets/leaves.jpg';
+import myLeavesImg from '../assets/myLeaves.jpg';
+/* eslint-enable */
+
 const theme = createMuiTheme(defaultTheme);
 
 const App: React.FC = () => {
