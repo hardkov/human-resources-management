7b87ff7d22f05fc16ea3e8a23e659bed5cccefe0
hardkov <krzysztofH150@interia.pl>
2021-06-08 02:30:12 +0200

    Prepared services for application managament

--- react-app/src/services/applicationService.ts
+++ react-app/src/services/applicationService.ts
@@ -5,16 +5,38 @@ import SubmitLeaveApplicationData from '../types/SubmitLeaveApplicationData';
 import SubmitDelegationApplicationData from '../types/SubmitDelegationApplicationData';
 import SubmitBonusApplicationData from '../types/SubmitBonusApplicationData';
 import LeaveApplicationData from '../types/LeaveApplicationData';
+import BonusApplicationData from '../types/BonusApplicationData';
+import DelegationApplicationData from '../types/DelegationApplicationData';
+
 import {
   LEAVE_APPLICATION_ENDPOINT,
   DELEGATION_APPLICATION_ENDPOINT,
   BONUS_APPLICATION_ENDPOINT,
   LEAVE_APPLICATION_MANAGEMENT_ENDPOINT,
+  BONUS_APPLICATION_MANAGEMENT_ENDPOINT,
+  DELEGATION_APPLICATION_MANAGEMENT_ENDPOINT,
+  DOWNLOAD_LEAVE_APPLICATION_ENDPOINT,
+  DOWNLOAD_BONUS_APPLICATION_ENDPOINT,
+  DOWNLOAD_DELEGATION_APPLICATION_ENDPOINT,
 } from './config';
+import ApplicationStatus from '../types/ApplicationStatus';
+import ApplicationType from '../types/ApplicationType';
 
 const getLeaveApplications = async (): Promise<ActionResult<LeaveApplicationData[]>> => {
   try {
     const response: AxiosResponse = await axios.get(LEAVE_APPLICATION_MANAGEMENT_ENDPOINT, {
+      headers: getAuthHeaders(),
+    });
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not fetch applications'] };
+  }
+};
+
+const getBonusApplications = async (): Promise<ActionResult<BonusApplicationData[]>> => {
+  try {
+    const response: AxiosResponse = await axios.get(BONUS_APPLICATION_MANAGEMENT_ENDPOINT, {
       headers: getAuthHeaders(),
     });
 
@@ -25,6 +47,54 @@ const getLeaveApplications = async (): Promise<ActionResult<LeaveApplicationData
     return { success: false, errors: ['Could not fetch applications'] };
   } catch {
     return { success: false, errors: ['Could not fetch applications'] };
+  }
+};
+
+const getDelegationApplications = async (): Promise<ActionResult<DelegationApplicationData[]>> => {
+  try {
+    const response: AxiosResponse = await axios.get(DELEGATION_APPLICATION_MANAGEMENT_ENDPOINT, {
+      headers: getAuthHeaders(),
+    });
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not fetch applications'] };
+  }
+};
+
+const getMyLeaveApplications = async (): Promise<ActionResult<LeaveApplicationData[]>> => {
+  try {
+    const response: AxiosResponse = await axios.get(LEAVE_APPLICATION_ENDPOINT, {
+      headers: getAuthHeaders(),
+    });
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not fetch applications'] };
+  }
+};
+
+const getMyBonusApplications = async (): Promise<ActionResult<BonusApplicationData[]>> => {
+  try {
+    const response: AxiosResponse = await axios.get(BONUS_APPLICATION_ENDPOINT, {
+      headers: getAuthHeaders(),
+    });
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not fetch applications'] };
+  }
+};
+
+const getMyDelegationApplications = async (): Promise<ActionResult<DelegationApplicationData[]>> => {
+  try {
+    const response: AxiosResponse = await axios.get(DELEGATION_APPLICATION_ENDPOINT, {
+      headers: getAuthHeaders(),
+    });
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not fetch applications'] };
   }
 };
 
@@ -78,4 +148,81 @@ const submitBonusApplication = async (bonusApplicationData: SubmitBonusApplicati
   }
 };
 
-export { submitLeaveApplication, submitDelegationApplication, submitBonusApplication, getLeaveApplications };
+const updateApplication = async (
+  id: number,
+  status: ApplicationStatus,
+  type: ApplicationType,
+): Promise<ActionResult> => {
+  let endpoint;
+
+  switch (type) {
+    case 'LEAVE':
+      endpoint = LEAVE_APPLICATION_MANAGEMENT_ENDPOINT;
+      break;
+    case 'BONUS':
+      endpoint = BONUS_APPLICATION_MANAGEMENT_ENDPOINT;
+      break;
+    case 'DELEGATION':
+      endpoint = DELEGATION_APPLICATION_MANAGEMENT_ENDPOINT;
+      break;
+    default:
+      endpoint = '';
+  }
+
+  try {
+    await axios.post(
+      endpoint,
+      { applicationId: id, status },
+      {
+        headers: getAuthHeaders(),
+      },
+    );
+
+    return { success: true };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not update application'] };
+  }
+};
+
+const downloadApplication = async (id: number, type: ApplicationType): Promise<ActionResult> => {
+  let endpoint;
+
+  switch (type) {
+    case 'LEAVE':
+      endpoint = DOWNLOAD_LEAVE_APPLICATION_ENDPOINT;
+      break;
+    case 'BONUS':
+      endpoint = DOWNLOAD_BONUS_APPLICATION_ENDPOINT;
+      break;
+    case 'DELEGATION':
+      endpoint = DOWNLOAD_DELEGATION_APPLICATION_ENDPOINT;
+      break;
+    default:
+      endpoint = '';
+  }
+
+  try {
+    await axios.get(`${endpoint}/${id}`, {
+      // params: { id },
+      headers: getAuthHeaders(),
+    });
+
+    return { success: true };
+  } catch (error) {
+    return { success: false, errors: error.response.data.errors || ['Could not download application'] };
+  }
+};
+
+export {
+  submitLeaveApplication,
+  submitDelegationApplication,
+  submitBonusApplication,
+  getLeaveApplications,
+  getBonusApplications,
+  getDelegationApplications,
+  getMyLeaveApplications,
+  getMyBonusApplications,
+  getMyDelegationApplications,
+  updateApplication,
+  downloadApplication,
+};
